6e502e336755e804e337b60cff51b016311c2fec
T Yoshizawa <t-yoshi@users.noreply.github.com>
2021-06-11 21:10:31 +0900

    シングルトンの必要はない

--- tv/src/main/java/org/peercast/core/tv/Module.kt
+++ tv/src/main/java/org/peercast/core/tv/Module.kt
@@ -1,7 +1,8 @@
 package org.peercast.core.tv
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val tvModule = module {
-    single { TvViewModel(get(), get()) }
+    viewModel { TvViewModel(get(), get()) }
 }
